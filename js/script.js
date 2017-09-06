85807d2b21c99cf3718f9990d8a7637dfa3509de
Heather Conley <hconley6695@gmail.com>
2017-09-05 22:38:07 -0400

    added Wiki ajax call and printed out articles on page

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ function loadData() {
 
     // NYTimes Ajax Request here
 
-    var url = "https://api.nyatimes.com/svc/search/v2/articlesearch.json";
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     var api_key = "2bd806ac49c342d6b4f15c34562bb856";
     var nyUrl = url + '?api-key=' + api_key + '&q='+ $city + "?";
 
@@ -47,6 +47,36 @@ function loadData() {
 
         $nytHeaderElem.text('NY Times articles could not be loaded.');
     }); //look at first example to get an idea, function will be run when response returns from the server
+
+
+    // WIKIPEDIA AJAX REQUEST
+
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + $city +'&format=json&callback=wikiCallback';
+    $.ajax( {
+        url: wikiUrl,
+        dataType: 'jsonp',
+        type: 'POST',
+        headers: { 'Api-User-Agent': 'Example/1.0' },
+        success: function(resp) {
+            // console.log(resp);
+
+            var listOfArticles = resp[1];
+            // console.log(listOfArticles);
+
+            listOfArticles.forEach(function(article) {
+
+                console.log(article);
+
+                var url = 'http://en.wikipedia.org/wiki/' + article;
+
+                $wikiElem.append('<li><a href="'+ url +'">' + article +'</a></li>');
+
+            })
+           // do something with data
+        }
+    } ).error(function() {
+        $wikiElem.append("wikipedia could not load");
+    });
 
 
     return false;
